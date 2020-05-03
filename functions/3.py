4e13b53408c650db09e073608e2c843e0ac26785
Aris <Yihui.19@intl.zju.edu.cn>
2020-05-03 16:54:07 +0800

    Function 3 corrected

--- functions/3.py
+++ functions/3.py
@@ -8,11 +8,11 @@ def Convertor(s:str):
     c= ''
     for i in range(0,s):
        if DNA[i] == 'A':
-          c = 'T' +c
-       elif DNA[i] == 'T':
           c = 'A' +c
+       elif DNA[i] == 'T':
+          c = 'U' +c
        elif DNA[i] == 'G':
-          c = 'C' +c
-       elif DNA[i] == 'C':
           c = 'G' +c
+       elif DNA[i] == 'C':
+          c = 'C' +c
     return (c)
